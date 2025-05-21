fc2e632d88a5b97a23def321e67179b45b11c30c
Marcelo D'almeida <marcelodalmeida42@gmail.com>
2025-05-21 00:25:35 -0300

    corrigindo bug de relacionamento

--- code/back/models/day.js
+++ code/back/models/day.js
@@ -1,41 +1,41 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+// const { DataTypes } = require('sequelize');
+// const sequelize = require('../config/database');
 
-const Reservation = require('./reservation').model;
+// const Reservation = require('./reservation').model;
 
-const Day = sequelize.define('Day', {
-  date: {
-    type: DataTypes.DATE,
-    allowNull: false
-  }
-}, {
-  tableName: 'days'
-});
+// const Day = sequelize.define('Day', {
+//   date: {
+//     type: DataTypes.DATE,
+//     allowNull: false
+//   }
+// }, {
+//   tableName: 'days'
+// });
 
-const Chef = sequelize.define('Chef', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  image: {
-    type: DataTypes.STRING,
-  },
-  isAvailable: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: true
-  }
-}, {
-  tableName: 'chefs'
-});
+// const Chef = sequelize.define('Chef', {
+//   name: {
+//     type: DataTypes.STRING,
+//     allowNull: false
+//   },
+//   image: {
+//     type: DataTypes.STRING,
+//   },
+//   isAvailable: {
+//     type: DataTypes.BOOLEAN,
+//     defaultValue: true
+//   }
+// }, {
+//   tableName: 'chefs'
+// });
 
-Day.hasMany(Chef, { foreignKey: 'dayId' });
-Chef.belongsTo(Day, { foreignKey: 'dayId' });
+// Day.hasMany(Chef, { foreignKey: 'dayId' });
+// Chef.belongsTo(Day, { foreignKey: 'dayId' });
 
-Chef.hasOne(Reservation, { foreignKey: 'chefId' });
-Reservation.belongsTo(Chef, { foreignKey: 'chefId' });
+// Chef.hasOne(Reservation, { foreignKey: 'chefId' });
+// Reservation.belongsTo(Chef, { foreignKey: 'chefId' });
 
-module.exports = {
-  Day,
-  Chef
-};
+// module.exports = {
+//   Day,
+//   Chef
+// };
 
